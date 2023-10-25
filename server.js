58ecc54876fb7f1e5965443b43f1543419eef04c
nathangero <nathangero@yahoo.com>
2023-10-25 00:34:36 -0700

    implement saving new note to db.json

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const path = require("path");
-const dbData = require("./db/db.json");
+const fs = require("fs");
+const db = require("./db/db.json");
 
 const PORT = 3001;
 
 const app = express();
+
+let dbData; // Will contain the data from db.json
 
 app.use(express.json()); // Needed for header 'Content-Type': 'application/json'
 app.use(express.static("public"));
@@ -18,16 +21,41 @@ app.get("/notes", (req, res) => {
 })
 
 app.get("/api/notes", (req, res) => {
-    res.send(dbData);
+    res.send(db);
 })
 
 // Saving a note
 app.post("/api/notes", (req, res) => {
     const body = req.body;
-    console.log("body:", body);
+    const title = body.title;
+    const text = body.text;
+
+    let note = {
+        title: title,
+        text: text
+    }
+
+    dbData.push(note);
+
+    fs.writeFile("./db/db.json", JSON.stringify(dbData, null, 4), (err) => {
+        err ? console.error(err) : console.log("added new note");
+        
+        // Send the note data regardless
+        res.send(db);
+    })
 })
 
 
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`)
+
+    // Read in json data when server starts
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
+        if (err) {
+            console.error(err);
+            return
+        }
+
+        dbData = JSON.parse(data);
+    })
 })
