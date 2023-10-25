813434a1b23699b690cc2bb993fbf0be915fb0e6
nathangero <nathangero@yahoo.com>
2023-10-24 23:50:35 -0700

    implement showing notes page

--- server.js
+++ server.js
@@ -12,8 +12,12 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "index.html"));
 })
 
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+})
+
 app.get("/api/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "notes.html"));
+    res.send(dbData);
 })
 
 
